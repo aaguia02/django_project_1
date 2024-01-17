9a9af8a0f469c630a73d141ec222f6fee22b2159
Alexandre Aguiar Kato <alexkato@yahoo.com>
2024-01-17 19:38:05 +0000

    initial commit

---
+++ proj1venv/hello_world/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+# Create your views here.
+
+# create first view
+def index(request):
+    if request.method == 'POST':
+        return HttpResponse("You must have POSTed something") 
+    elif request.method == 'GET':
+        return HttpResponse(request.method)
+q
